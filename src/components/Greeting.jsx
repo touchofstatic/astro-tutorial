201028032b48404dd268167a9dcb50821658d8b6
knifecentipede <eugenefishberg@gmail.com>
2024-06-25 15:12:08 +0300

    Improved theme button

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'preact/hooks';
 
-export default function Greeting({messages}) {
+export default function Greeting({ messages }) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
 
@@ -9,13 +9,14 @@ export default function Greeting({messages}) {
   return (
     <div>
 
+      {/* FIX THIS WRAPPING!!! */}
       <h3 style="text-wrap:balance">
         <div style="white-space:nowrap">Thank you for visiting! </div>
         {greeting}
       </h3>
-      
+
       <button
-      onClick={() => setGreeting(randomMessage())}>
+        onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
 
