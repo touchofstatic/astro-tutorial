39939a6b59152dfc19a4dfea4e5c4187b1923319
knifecentipede <eugenefishberg@gmail.com>
2024-07-19 16:30:42 +0300

    Astro tutorial: added blog post reading time with remark

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   site: "https://thisismyfirstrodeo.netlify.app/",
   integrations: [preact()]
 });
