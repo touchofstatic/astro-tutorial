412676343a04accb5c27939177b0c6243d052578
knifecentipede <eugenefishberg@gmail.com>
2025-01-11 11:10:29 +0200

    Back at it again with astrodb

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   site: "https://thisismyfirstrodeo.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), db()]
 });
