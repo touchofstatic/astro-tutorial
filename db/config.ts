295b2ba15523f786144ca5d537146968482e0dbf
knifecentipede <eugenefishberg@gmail.com>
2025-01-11 11:25:50 +0200

    turso comments branch test

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,16 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
-// https://astro.build/db/config
+const Comment = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    postSlug: column.text(),
+    name: column.text(),
+    email: column.text(),
+    message: column.text(),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Comment },
 });
