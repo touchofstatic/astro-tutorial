2aa3988d193af05ee61336995bb66274c2d744fb
touchofstatic <eugenefishberg@gmail.com>
2025-05-18 17:27:32 +0300

    test

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,20 @@
-import { db } from 'astro:db';
+import { db, Comment } from 'astro:db';
 
-// https://astro.build/db/seed
-export default async function seed() {
-	// TODO
+export default async function () {
+  await db.insert(Comment).values([
+    {
+      postSlug: 'post01',
+      name: 'Jamie',
+      email: 'jamie@turso.tech',
+      message: 'Great post!',
+      createdAt: new Date(),
+    },
+    {
+      postSlug: 'post02',
+      name: 'Jamie',
+      email: 'jamie@turso.tech',
+      message: 'Another great post!',
+      createdAt: new Date(),
+    },
+  ]);
 }
